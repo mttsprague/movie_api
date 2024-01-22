493aa5546f88dcea19cfd9213f52ee2f20346602
mttsprague <mttsprague@gmail.com>
2024-01-22 11:27:00 -0600

    error handling middleware
    error handling

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(morgan('common'));
 
+app.use ((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 let topMovies = [
     {
         title: 'Top Gun',
